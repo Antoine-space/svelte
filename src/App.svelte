27e77ba555c42e9e7679267804079a772c135e57
Antoine-space <hunter.monster070@gmail.com>
2021-12-29 10:50:41 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	let name = 'world';
+	let tableau = [ { id : 1, value : 10},
+					{ id : 2, value : 13},
+					{ id : 3, value : 20} ];
+</script>
+
+<h1>Hello {name}!</h1>
+
+<ul>
+{#each tableau as lignes }
+	<li>{lignes}</li>	
+{/each}
+</ul>
